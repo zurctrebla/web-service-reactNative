9c8aa143af91ef1b23813ec92ba9fca1afa30e67
zurctrebla <albertcruz@terra.com.br>
2022-11-12 13:58:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);    
+    
+    fetch(`https://api.tvmaze.com/search/shows?q=${data.tracking}`)
+
+    .then(response => response.json())
+
+    .then(console.log)
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container">
+      <h1>Consultar Filmes</h1>
+      <form onSubmit={submitHandler}>
+        <div className="form-group">
+          <input type="text" className="form-control" name="tracking" placeholder="nome do filme"/>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Buscar
+        </button>
+      </form>
+
+      { setData }
+
     </div>
   );
 }
